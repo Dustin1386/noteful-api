90d61311ad6b31aa95f742a49e6792fc84834c55
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-11-03 14:38:12 -0700

    seed files added

---
+++ seeds/seed.notes.sql
@@ -0,0 +1,5 @@
+INSERT INTO notes (name, modified, folder_id, content)
+VALUES
+    ('Dolphins', now(), '1', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
+    ('Bats', now(), '2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?'),
+    ('Bears', now(), '3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non. Adipisci, pariatur. Molestiae, libero esse hic adipisci autem neque?');
