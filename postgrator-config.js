7bf110333d1081b39a5f6bf18c81018c98df0d9d
DUSTIN CHESSNUTT <dustinchesnutt@yahoo.com>
2020-11-03 16:39:23 -0700

    heroku stuff

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
-module.exports = {
-    "migrationDirectory": "migrations",
-    "driver": "pg",
-    "connectionString": (process.env.NODE_ENV === 'test')
-     ? process.env.TEST_DB_URL : process.env.DB_URL,
 
-  }
+module.exports = {
+  "migrationDirectory": "migrations",
+  "driver": "pg",
+  "host": process.env.PROD_MIGRATION_DB_HOST,
+  "port": process.env.PROD_MIGRATION_DB_PORT,
+  "database": process.env.PROD_MIGRATION_DB_NAME,
+  "username": process.env.PROD_MIGRATION_DB_USER,
+  "password": process.env.PROD_MIGRATION_DB_PASS,
+  "ssl": true
+}
